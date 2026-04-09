39d11543b046f6c5e330c52faa3d6c1fb08ec4d3
Bhushan Dixit <bdixit@xtsworld.in>
2026-04-09 19:31:10 +0530

    Initial setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      lines: 80,
+      functions: 80,
+      branches: 80,
+      statements: 80,
+    },
+  },
 })
