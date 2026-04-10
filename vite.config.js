147ffc7da2b8641648d3d8a9a57a39ea1fa9984b
Bhushan Dixit <bdixit@xtsworld.in>
2026-04-10 18:30:11 +0530

    Updated the YML file for the complete CI/CD flow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -10,11 +9,14 @@ export default defineConfig({
     setupFiles: "./src/test/setup.ts",
     coverage: {
       provider: "v8",
-      reporter: ["text", "html"],
-      lines: 80,
-      functions: 80,
-      branches: 80,
-      statements: 80,
+      reporter: ["text", "html", "json-summary", "lcov"],
+      reportsDirectory: "./coverage",
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
     },
   },
-})
+});
