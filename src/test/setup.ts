a112e72f3ea7729f0394d9ec6e7ff87e4137d89c
Bhushan Dixit <bdixit@xtsworld.in>
2026-04-10 18:32:12 +0530

    Remaining setup.ts file updated

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,12 @@
-import "@testing-library/jest-dom";
+// FILE: src/test/setup.js
+
+import '@testing-library/jest-dom'
+
+// Prevent mocks from leaking between tests
+beforeEach(() => {
+  vi.clearAllMocks()
+})
+
+afterEach(() => {
+  vi.restoreAllMocks()
+})
